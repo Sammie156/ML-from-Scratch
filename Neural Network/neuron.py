9a44fbf54594ea2b085db9914f54de2ddd5ec615
Sammie156 <samanwayadatta@gmail.com>
2026-09-11 11:11:48 +0530

    Basic implementation of Neuron

--- Neural Network/neuron.py
+++ Neural Network/neuron.py
@@ -7,10 +7,21 @@ class Neuron:
         self.w = [Value(random.uniform(-1, 1)) for _ in range(n_inputs)]
         self.b = Value(0.0)
 
+    def parameters(self):
+        parameter = []
+
+        for w in self.w:
+            parameter.append(w)
+
+        parameter.append(self.b)
+
+        return parameter
+
     def __call__(self, x):
         result = Value(0.0)
-        for i in range(len(x)):
-            result += self.w[i] * x[i]
+
+        for wi, xi in zip(self.w, x):
+            result += wi * xi
 
         result += self.b
         result = result.tanh()
